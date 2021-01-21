40fb9f555956d2dfc8e2566011f46ead43657585
Christopher Bergman <cb.christopherbergman@gmail.com>
2021-01-21 12:06:52 +0100

    Lagt till så man kan skapa nya blogg-inlägg.

--- script.js
+++ script.js
@@ -32,26 +32,55 @@ function readMore(blog) {
 
 
 
-let blogPosts =  document.querySelector(".blogPosts");
+let blogPosts =  document.querySelector(".blogPosts__items");
+let addHeader = document.querySelector(".addHeader");
 let addText = document.querySelector(".addText");
 let addPostBtn = document.querySelector(".addPostBtn");
 
 
-addPostBtn.addEventListener('click', function() {
-    //skapade ett nytt "kort" som tar in elementet paragraf. 
-    let newCard = document.createElement('p');
-    //Gjorde så den input som användaren skrev i inputen hamnar i newcardContent.
-    let newCardContent = document.createTextNode(addText.value);
 
-    //lägga in input från användaren i taggen jag skapat ovan.
+addPostBtn.addEventListener('click', function() {
+    //skapade ett nytt "kort" som tar in elementet paragraf.
+    let newDiv = document.createElement('div');
+    let newHeader = document.createElement('h2'); 
+    let newCard = document.createElement('p');
+    let newEditBtn = document.createElement('button');
+    
+    //Gjorde så den input som användaren skrev i inputen hamnar i newcardContent.
+    let newCardHeader = document.createTextNode(addHeader.value);
+    let newCardContent = document.createTextNode(addText.value);
+    let newEditBtnContent = document.createTextNode("Edit");
+
+    
+
+    //lägga in input från användaren i kort-taggen.
     newCard.appendChild(newCardContent);
+    newHeader.appendChild(newCardHeader);
+    newEditBtn.appendChild(newEditBtnContent);
+    
+
+    //appendar in varje element in i div taggen.
+    newDiv.appendChild(newHeader);
+    newDiv.appendChild(newCard);
+    newDiv.appendChild(newEditBtn);
+
+    //Skapar class namn till min div som skapas. 
+    newDiv.className = "card";
 
     /*lägga till denna tag som har all info ovan inuti sig i hemsidan efter att man
-     trycker på knappen */
-    blogPosts.appendChild(newCard);
-
+     trycker på post knappen. */
+     blogPosts.prepend(newDiv);
+    
     //TODO: Behöver nu göra så den skapar samma design som mina inlägg med rubrik + paragraf
     //TODO: samt samma layout som de har. 
     
 });
+
+// Skapa en event när man trycker edit skall inlägget kunna bli redigerat.
+
+editBtn.addEventListener('click', function {
+
+});
+
+
 
